9854fbd8607c0cc6e79bb72ee1c6011e8a34b0c5
marcosgabrielgreco <165661707+marcosgabrielgreco@users.noreply.github.com>
2026-08-27 09:34:07 -0300

    s

--- script.js
+++ script.js
@@ -41,3 +41,24 @@ const perguntas = [
         ]
     }
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+/*aula4*/
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
